e04ca8333e7d06d69b5b38374dc9fe9514a1c27d
Sukhmeet <sukhbawa2049@gmail.com>
2022-08-23 17:20:48 +0530

    Fix Repetition of path and configFile

--- src/GetSetData.java
+++ src/GetSetData.java
@@ -2,17 +2,15 @@ import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.Properties;
-
 public class GetSetData {
 
-    String path = System.getProperty("user.home") + File.separator + ".lanMessenger";
-    File configFile = new File(path + File.separator + "config.properties");
+
     Properties properties = new Properties();
     FileReader reader;
     FileWriter writer;
     public String getUsername(){
         try {
-            reader = new FileReader(configFile);
+            reader = new FileReader(Main.configFile);
             properties.load(reader);
             return properties.getProperty("USERNAME");
         } catch (IOException e) {
@@ -22,7 +20,7 @@ public class GetSetData {
 
     public int getFontSize(){
         try {
-            reader = new FileReader(configFile);
+            reader = new FileReader(Main.configFile);
             properties.load(reader);
             if (properties.getProperty("FONTSIZE") != null)
                 return Integer.parseInt(properties.getProperty("FONTSIZE"));
@@ -35,7 +33,7 @@ public class GetSetData {
     public void setUsername(String username){
         properties.setProperty("USERNAME", username);
         try {
-            writer = new FileWriter(configFile);
+            writer = new FileWriter(Main.configFile);
             properties.store(writer, null);
             writer.close();
         } catch (IOException e) {
@@ -47,7 +45,7 @@ public class GetSetData {
 
         properties.setProperty("FONTSIZE", fontSize);
         try {
-            writer = new FileWriter(configFile);
+            writer = new FileWriter(Main.configFile);
             properties.store(writer, null);
             writer.close();
         } catch (IOException e) {
